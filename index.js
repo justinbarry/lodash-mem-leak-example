780352f8c758d08be19103902edcf4224f3af40c
Justin Barry <>
2018-05-16 21:00:11 -0700

    Create test case to demo memory leak

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const profiler = require('v8-profiler-node8');
+
+const lodash = require('lodash');
+
+const set = require('lodash.set');
+const unset = require('lodash.unset');
+
+
+const runIt = (setFn, unsetFn, runName) => {
+  const snapshot1 = profiler.takeSnapshot();
+  snapshot1.export(function(error, result) {
+    fs.writeFileSync(`${runName}-1.heapsnapshot`, result);
+    snapshot1.delete();
+  });
+
+  const obj = {};
+
+  const numCycles = 10000;
+
+  for (let i = 0; i < numCycles; i++) {
+    let fillerObj = {
+      id: i,
+    };
+
+    setFn(obj, "testKey" + i, fillerObj);
+  }
+  for (let i = 0; i < numCycles; i++) {
+    unsetFn(obj, "testKey" + i);
+  }
+
+  const snapshot2 = profiler.takeSnapshot();
+  snapshot2.export(function(error, result) {
+    fs.writeFileSync(`${runName}-2.heapsnapshot`, result);
+    snapshot2.delete();
+  });
+}
+
+runIt(lodash.set, lodash.unset, 'single-package');
+runIt(set, unset, 'separate-packages');
